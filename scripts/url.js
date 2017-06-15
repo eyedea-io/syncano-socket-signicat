9ef4bcee7399c73b5872210e1c66e601c01f01fc
mkucharz <m@kucharz.net>
2017-06-15 15:14:07 +0200

    Fixinggetting args.

--- scripts/url.js
+++ scripts/url.js
@@ -1,9 +1,9 @@
 import { response } from 'syncano-server'
 
 
-const additionalArgs = Object.keys(ARGS.args).map(k => k + '=' + encodeURIComponent(ARGS.args[k])).join('&')
+const additionalArgs = Object.keys(ARGS.args || {}).map(k => k + '=' + encodeURIComponent(ARGS.args[k])).join('&')
 
 response.json({
   url: `https://${CONFIG.HOST}/std/method/demo?id=${ARGS.method}:${CONFIG.SERVICE}:${ARGS.lang}&
-target=https://${META.instance}.syncano.space/signicat/verify/${additionalArgs}`
+target=https://${META.instance}.syncano.space/signicat/verify/${additionalArgs ? `?${additionalArgs}` : ''}`
 })
